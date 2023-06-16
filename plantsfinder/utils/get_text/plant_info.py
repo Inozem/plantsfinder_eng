8d3a0b792162c2d00e30c64e038690cea00bc30c
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2023-06-16 15:49:17 +0300

    Tranclated

--- plantsfinder/utils/get_text/plant_info.py
+++ plantsfinder/utils/get_text/plant_info.py
@@ -22,42 +22,39 @@ def get_plant_soil_types(plant):
     """Returns soil types of the plant."""
     soil_types = [soil.name for soil in plant.soil_type.all()]
     soil_types += [soil.name for soil in plant.soil_fertility.all()]
-    text = ', '.join(soil_types[:-1]) + ' и ' + soil_types[-1]
+    text = ', '.join(soil_types[:-1]) + ' and ' + soil_types[-1]
     return text.lower()
 
 
 def get_plant_soil_ph_min_max(plant):
-    """Returns the minimum and maximum optimal level of soil pH of the plant."""
+    """
+    Returns the minimum and maximum optimal level of soil pH of the plant.
+    """
     soil_ph = sorted([ph.name for ph in plant.soil_ph.all()])
     return f'{soil_ph[0]}-{soil_ph[-1]}'
 
 
 def get_plant_soil_moisture(plant):
     """Returns soil moisture types of the plant."""
-    extreme_moisture = {
-        'Возможна засуха': 'засуху',
-        'Возможны затопления': 'затопления',
-    }
-    moisture = [ph.name for ph in plant.soil_moisture.all()]
+    extreme_moisture = ['Drought possible', 'Flooding possible']
+    moisture = [moisture.name for moisture in plant.soil_moisture.all()]
     soil_moisture = []
     soil_extreme_moisture = []
     for moisture_value in moisture:
         if moisture_value not in extreme_moisture:
             soil_moisture.append(moisture_value)
         else:
-            soil_extreme_moisture.append(moisture_value)
+            soil_extreme_moisture.append(moisture_value.split(' ')[0])
     text = ''
     if len(soil_moisture) > 1:
         text += ', '.join(soil_moisture[:-1])
-        text += f' и {soil_moisture[-1]}'
+        text += f' and {soil_moisture[-1]}'
     elif len(soil_moisture) == 1:
         text += soil_moisture[0]
     if len(soil_extreme_moisture) > 0:
-        for ind, moisture_name in enumerate(soil_extreme_moisture):
-            soil_extreme_moisture[ind] = extreme_moisture[moisture_name]
-        text += ', может переносить '
+        text += ', can tolerate '
         if len(soil_extreme_moisture) == 1:
             text += soil_extreme_moisture[0]
         else:
-            text += ' и '.join(soil_extreme_moisture)
+            text += ' and '.join(soil_extreme_moisture)
     return text.lower()
