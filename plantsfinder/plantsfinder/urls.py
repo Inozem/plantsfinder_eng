64d9468cd96f4fd160ebe0da8bf661c1533802b0
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2023-06-23 22:27:58 +0300

    Added main page

--- plantsfinder/plantsfinder/urls.py
+++ plantsfinder/plantsfinder/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('plants/', include('plants.urls', namespace='plants')),
+    path('', views.main_page, name='main_page'),
 ]
 
 if settings.DEBUG:
